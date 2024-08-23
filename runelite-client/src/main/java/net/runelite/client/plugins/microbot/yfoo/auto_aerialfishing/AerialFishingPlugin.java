4c47a95003611e1bbd2de37a4ef99dd6ba5a3294
yfoo <yfoo@protonmail.com>
2024-08-22 23:12:48 -0700

    add hybrid fillet + config menu

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/yfoo/auto_aerialfishing/AerialFishingPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/yfoo/auto_aerialfishing/AerialFishingPlugin.java
@@ -8,6 +8,7 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.yfoo.Task.Task;
 import net.runelite.client.plugins.microbot.yfoo.auto_aerialfishing.TaskSubclasses.CatchAerialFish;
 import net.runelite.client.ui.overlay.OverlayManager;
@@ -42,16 +43,23 @@ public class AerialFishingPlugin extends Plugin {
 
     @Override
     protected void startUp() throws Exception {
+        if(!config.allowFastFillet() && !config.allowSlowFillet()) {
+            Microbot.log("One of Fast/Slow fillet must be checked!. Reset then restart the script.");
+            return;
+        }
+
         log.info("Aerial Fishing Plugin started!");
+        AerialFishingOverlay.resetStats();
         overlayManager.add(aerialFishingOverlay);
-        this.aerialFishingScript = new AerialFishingScript();
-        aerialFishingScript.run(config);
+        this.aerialFishingScript = new AerialFishingScript(config);
+        aerialFishingScript.run();
     }
 
     @Override
     protected void shutDown() throws Exception {
         log.info("Aerial Fishing Plugin stopped!");
-        aerialFishingScript.shutdown();
+        if(aerialFishingScript != null)
+            aerialFishingScript.shutdown();
         overlayManager.remove(aerialFishingOverlay);
         Task.cleanupTasks();
     }
--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/yfoo/auto_aerialfishing/AerialFishingScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/yfoo/auto_aerialfishing/AerialFishingScript.java
@@ -6,18 +6,20 @@ import net.runelite.client.plugins.microbot.yfoo.Task.Task;
 import net.runelite.client.plugins.microbot.yfoo.auto_aerialfishing.TaskSubclasses.CatchAerialFish;
 import net.runelite.client.plugins.microbot.yfoo.auto_aerialfishing.TaskSubclasses.FilletAerialFish;
 
+import javax.inject.Inject;
 import java.util.concurrent.TimeUnit;
 
 public class AerialFishingScript extends Script {
 
-    public static final String version = "0.18";
+    public static final String version = "0.20";
 
-    public AerialFishingScript() {
-        FilletAerialFish.initInstance(this);
+
+    public AerialFishingScript(AerialFishingConfig config) {
+        FilletAerialFish.initInstance(this, config);
         CatchAerialFish.initInstance(this);
     }
 
-    public boolean run(AerialFishingConfig config) {
+    public boolean run() {
         Task.stopScriptNow = false;
         mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
             if (!super.run() || !Microbot.isLoggedIn()) {
@@ -28,8 +30,8 @@ public class AerialFishingScript extends Script {
             try {
                 Task.runLoopIteration(this);
             } catch (Exception e) {
-                Microbot.log(e.getMessage());
-                throw new RuntimeException(e);
+                Microbot.log("Encountered Exception: " + e.getMessage());
+                this.shutdown();
             }
         }, 0, 600, TimeUnit.MILLISECONDS);
         return true;
