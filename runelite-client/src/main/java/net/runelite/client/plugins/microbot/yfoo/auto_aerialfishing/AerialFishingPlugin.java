0e7da099c4a31acfcb114558f457989df05aeba4
Yifan <yifanli722@gmail.com>
2024-09-09 11:23:59 -0700

    update aerial fishing

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/yfoo/auto_aerialfishing/AerialFishingPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/yfoo/auto_aerialfishing/AerialFishingPlugin.java
@@ -2,6 +2,7 @@ package net.runelite.client.plugins.microbot.yfoo.auto_aerialfishing;
 
 import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.EquipmentInventorySlot;
 import net.runelite.api.InventoryID;
 import net.runelite.api.events.ItemContainerChanged;
 import net.runelite.client.config.ConfigManager;
@@ -9,6 +10,8 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.plugins.microbot.util.equipment.Rs2Equipment;
+import net.runelite.client.plugins.microbot.util.inventory.Rs2Item;
 import net.runelite.client.plugins.microbot.yfoo.Task.Task;
 import net.runelite.client.plugins.microbot.yfoo.auto_aerialfishing.TaskSubclasses.CatchAerialFish;
 import net.runelite.client.ui.overlay.OverlayManager;
@@ -17,7 +20,7 @@ import javax.inject.Inject;
 
 
 @PluginDescriptor(
-        name = "Auto Aerial Fishing",
+        name = PluginDescriptor.yfoo + "Auto Aerial Fishing",
         description = "Automates aerial fishing on molch island",
         tags = {"fishing", "aerial", "microbot"},
         enabledByDefault = false
@@ -45,6 +48,12 @@ public class AerialFishingPlugin extends Plugin {
     protected void startUp() throws Exception {
         if(!config.allowFastFillet() && !config.allowSlowFillet()) {
             Microbot.log("One of Fast/Slow fillet must be checked!. Reset then restart the script.");
+            return;
+        }
+        Rs2Item equippedWeapon = Rs2Equipment.get(EquipmentInventorySlot.WEAPON);
+        int weaponId = equippedWeapon != null ? equippedWeapon.getId() : -1;
+        if(weaponId != 22817) {
+            Microbot.log("Player is not wielding the bird. Get one from the Alry the Angler then restart the script.");
             return;
         }
 
